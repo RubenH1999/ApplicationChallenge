a4f9866b73721198ae73b30fc3a51f9e0181a54c
stefmarien95 <56428152+stefmarien95@users.noreply.github.com>
2019-12-09 13:46:44 +0100

    zrvzrvg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatCardModule, MatTabsModule } from '@angular/material';
 import { TagInputModule } from 'ngx-chips';
 import { FormsModule } from '@angular/forms';
