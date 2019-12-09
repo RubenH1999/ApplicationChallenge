062c336d003a9b7433b5bbba7372bb68b78e787b
Tijl Ingelaere <r0579290@student.thomasmore.be>
2019-12-09 14:19:39 +0100

    Searchfunction maker WIP

--- src/app/models/bedrijf.model.ts
+++ src/app/models/bedrijf.model.ts
@@ -1,3 +1,3 @@
 export class Bedrijf {
-    constructor(public bedrijfID:number, public locatie: string,public tags:string,public biografie:string, public telefoon:string){}
+    constructor(public bedrijfID: number, public locatie: string,public tags: string,public biografie: string, public telefoon: string, public account: Account){}
 }
