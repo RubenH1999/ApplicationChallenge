55fbe93f7d6a34366aab945ac2792f9cfa6fecc7
Tijl Ingelaere <r0579290@student.thomasmore.be>
2019-12-09 15:31:51 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/maker/bedrijf-detail/bedrijf-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bedrijf-detail',
+  templateUrl: './bedrijf-detail.component.html',
+  styleUrls: ['./bedrijf-detail.component.css']
+})
+export class BedrijfDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
