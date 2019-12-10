3fde2c1db501370e8304cbeec654270c2b9f74ca
unknown <r0706810@student.thomasmore.be>
2019-12-10 11:15:45 +0100

    merge conflict admin

--- src/app/admin/maker-toevoegen/maker-toevoegen.component.ts
+++ src/app/admin/maker-toevoegen/maker-toevoegen.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
+
 
 import { Maker } from 'src/app/models/maker.model';
 import { Gebruiker } from 'src/app/models/gebruiker.model';
 import { Rol } from 'src/app/models/rol.model';
-=======
-import { Maker } from 'src/app/models/maker.model';
-import { Gebruiker } from 'src/app/models/gebruiker.model';
 
->>>>>>> d9af14931e344834c6a8d5ba4348e03fb55e4498
+
 
 @Component({
   selector: 'app-maker-toevoegen',
@@ -17,11 +14,11 @@ import { Gebruiker } from 'src/app/models/gebruiker.model';
 })
 export class MakerToevoegenComponent implements OnInit {
   model:Maker=new Maker(0,"","",false,"","","")
-<<<<<<< HEAD
+
   modelGebruiker:Gebruiker=new Gebruiker(0,"","",1)
-=======
+
   modelAccount:Gebruiker=new Gebruiker(0,"","",0)
->>>>>>> d9af14931e344834c6a8d5ba4348e03fb55e4498
+
   submitted : boolean = false;
 
   constructor() { }
@@ -30,7 +27,7 @@ export class MakerToevoegenComponent implements OnInit {
   }
   onSubmit() {
     //voeg hier een maker en een maker account toe
-
+    //Waardeloze commentaar in een poging github te laten werken
   }
 
 }
