8b05275fa7446d07b0eab8363c745196e87aa9d1
Tijl Ingelaere <r0579290@student.thomasmore.be>
2019-12-11 18:13:07 +0100

    Fixed search

--- src/app/models/maker.model.ts
+++ src/app/models/maker.model.ts
@@ -3,7 +3,5 @@ import { Assignment } from './assignment.model';
 import { TagMaker } from './tag-maker.model';
 
 export class Maker {
-
 constructor(public makerID: number, public geboorteDatum: string, public biografie: string, public student: boolean, public linkedIn: string, public ervaring: string,public telefoonNummer: number, public gebruikerID: number,public gebruiker?: Gebruiker, public assignments?: Array<Assignment>, public tagMakers?: Array<TagMaker>){}
-
 }
