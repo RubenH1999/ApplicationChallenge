d48ed5268ed275bbec4a94c7ad212bc8d219381d
Tijl Ingelaere <r0579290@student.thomasmore.be>
2019-12-13 15:15:15 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/models/geintereseerd-assignment.model.spec.ts
@@ -0,0 +1,7 @@
+import { GeintereseerdAssignment } from './geintereseerd-assignment.model';
+
+describe('GeintereseerdAssignment', () => {
+  it('should create an instance', () => {
+    expect(new GeintereseerdAssignment()).toBeTruthy();
+  });
+});
