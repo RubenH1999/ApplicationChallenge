174f8c5c5860942c92a4080519914132259361d4
Tijl Ingelaere <r0579290@student.thomasmore.be>
2019-12-15 15:05:54 +0100

    fix error

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,17 +11,7 @@ import { BedrijfToevoegenComponent } from './bedrijf-toevoegen/bedrijf-toevoegen
 import { HttpClientModule } from '@angular/common/http';
 import { AdminMakerDetailComponent } from './admin-maker-detail/admin-maker-detail.component';
 import { AdminBedrijfDetailComponent } from './admin-bedrijf-detail/admin-bedrijf-detail.component';
-<<<<<<< HEAD
 import { AdminGuard } from '../auth/admin.guard'
-
-const appRoutes: Routes = [
-  { path: 'adminMaker', component: AdminMakerComponent ,canActivate:[AdminGuard]},
-  { path: 'voegMakerToe', component: MakerToevoegenComponent,canActivate:[AdminGuard] },
-  { path: 'detailMaker/:id', component: AdminMakerDetailComponent,canActivate:[AdminGuard] },
-  { path: 'adminBedrijf', component: AdminBedrijvenComponent ,canActivate:[AdminGuard]},
-  { path: 'voegBedrijfToe', component: BedrijfToevoegenComponent,canActivate:[AdminGuard] },
-  { path: 'detailBedrijf/:id', component: AdminBedrijfDetailComponent,canActivate:[AdminGuard] },
-=======
 import { AdminBedrijfReviewComponent } from './admin-bedrijf-review/admin-bedrijf-review.component';
 
 const appRoutes: Routes = [
@@ -32,8 +22,6 @@ const appRoutes: Routes = [
   { path: 'voegBedrijfToe', component: BedrijfToevoegenComponent },
   { path: 'detailBedrijf/:id', component: AdminBedrijfDetailComponent },
   { path: 'bedrijfReview/:id', component: AdminBedrijfReviewComponent },
->>>>>>> 3eb4eaf580b8e4d9431b85ae2527b7be85f304d3
- 
 ];
 
 @NgModule({
